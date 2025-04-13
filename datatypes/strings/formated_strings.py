7f884d25908a218207538c205a7636fbc25777f0
mrinaljain <jain.mrinal140@gmail.com>
2025-04-13 16:52:22 -0400

    added a guessing gape in python

--- datatypes/strings/formated_strings.py
+++ datatypes/strings/formated_strings.py
@@ -9,7 +9,7 @@ print(str5)
 
 
 # furthur if we want to format the placeholder value 
-
+# A modifier is included by adding a colon : followed by a legal formatting type, like .2f which means fixed point number with 2 decimals
 
 str6 = f"This is a super formates {nu:.2f} string"
 
--- pip/pip.py
+++ pip/pip.py
@@ -3,5 +3,5 @@ import camelcase
 c = camelcase.CamelCase()
 
 txt = "hello world"
-
+m = c.hump(txt)
 print(c.hump(txt))
